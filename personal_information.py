82d6a49e44f4c8950ba99f380e077f6cfb3fc818
hemantacharya1 <hemantacharya882@gmail.com>
2025-07-23 22:34:08 +0530

    personal information file completed

---
+++ personal_information.py
@@ -0,0 +1,4 @@
+name,age,city,hobby=input("Enter your full name: "),input("Enter your age: "),input("Enter your city: "),input("Enter your hobby: ")
+print("Hello", name ,"!")
+print("You are ",age,"years old and live in",city,".")
+print("In your free time, you enjoy ",hobby)
